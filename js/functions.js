de93be72a512bc3e1c5ff62d5f6c0c59d3db64a0
Emppuuh <emilia_kamunen@hotmail.com>
2024-09-11 19:12:22 +0300

    Guess number form

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+'use script'
+
+const getRandomInNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+document.querySelector('button').addEventListener('click',() => {
+    const randomized_number = getRandomInNumberInRange(1,6)
+    const input = document.querySelector('input')
+    const guess = Number(input.value)
+
+    if (guess === randomized_number) {
+        alert("You guessed right, number is " + randomized_number)
+    } else {
+        alert("Your guess is wrong, number is " + randomized_number)
+    }
+
+    input.value = ''
+    input.focus
+})
